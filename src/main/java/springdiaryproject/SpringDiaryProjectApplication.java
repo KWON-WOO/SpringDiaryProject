e9d40cc573b80d186cecbb912606d3578153ebcb
KWON-WOO <ckw075@naver.com>
2025-11-04 14:15:07 +0900

    Initial commit

---
+++ src/main/java/springdiaryproject/SpringDiaryProjectApplication.java
@@ -0,0 +1,13 @@
+package springdiaryproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDiaryProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDiaryProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springdiaryproject/SpringDiaryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package springdiaryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDiaryProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
